19792621627250504a1135400926321245f16184
Abdullah Hashim <roxas0zero@gmail.com>
2020-03-23 01:18:47 +0300

    more configurations

--- ConfiguringApps/Controllers/HomeController.cs
+++ ConfiguringApps/Controllers/HomeController.cs
@@ -1,25 +1,47 @@
 ﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using ConfiguringApps.Infrastructure;
+using System;
+using Microsoft.Extensions.Logging;
 
 namespace ConfiguringApps.Controllers
 {
     public class HomeController : Controller
     {
         private readonly UptimeService uptime;
+        private readonly ILogger<HomeController> logger;
 
-        public HomeController(UptimeService uptime)
+        public HomeController(UptimeService uptime, ILogger<HomeController> logger)
         {
             this.uptime = uptime;
+            this.logger = logger;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(bool throwException = false)
         {
+            logger.LogDebug($"Handled {Request.Path} at uptime {uptime.Uptime}ms");
+
+            if (throwException)
+            {
+                throw new NullReferenceException();
+            }
+
             var model = new Dictionary<string, string>
             {
                 ["Message"] = "This is the Index action",
                 ["Uptime"] = $"{uptime.Uptime}ms"
             };
+
+            return View(model);
+        }
+
+        public IActionResult Error()
+        {
+            var model = new Dictionary<string, string>
+            {
+                ["Message"] = "This is Error action"
+            };
+
             return View(model);
         }
     }
--- ConfiguringApps/Startup.cs
+++ ConfiguringApps/Startup.cs
@@ -1,6 +1,7 @@
 using ConfiguringApps.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -8,27 +9,59 @@ namespace ConfiguringApps
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public Startup(IConfiguration configuration)
         {
-            services.AddControllersWithViews();
-            services.AddSingleton<UptimeService>();
+            Configuration = configuration;
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<UptimeService>();
+            services.AddControllersWithViews().AddMvcOptions(options =>
+                        options.RespectBrowserAcceptHeader = true);
+        }
+
+        public void ConfigureDevelopmentServices(IServiceCollection services)
+        {
+            services.AddSingleton<UptimeService>();
+            services.AddControllersWithViews();
+        }
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if ((Configuration.GetSection("ShortCircuitMiddleware")?
+                .GetValue<bool>("EnableBrowserShortCircuit")).Value)
             {
-                app.UseDeveloperExceptionPage();
+                app.UseMiddleware<BrowserTypeMiddleware>();
+                app.UseMiddleware<ShortCircuitMiddleware>();
             }
-
+            app.UseExceptionHandler("/Home/Error");
             app.UseMiddleware<ErrorMiddleware>();
-            app.UseMiddleware<BrowserTypeMiddleware>();
-            app.UseMiddleware<ShortCircuitMiddleware>();
             app.UseMiddleware<ContentMiddleware>();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
 
+        public void ConfigureDevelopment(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if ((Configuration.GetSection("ShortCircuitMiddleware")?
+                .GetValue<bool>("EnableBrowserShortCircuit")).Value)
+            {
+                app.UseMiddleware<BrowserTypeMiddleware>();
+                app.UseMiddleware<ShortCircuitMiddleware>();
+            }
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseBrowserLink();
+            app.UseMiddleware<ErrorMiddleware>();
+            app.UseMiddleware<ContentMiddleware>();
+            app.UseStaticFiles();
             app.UseRouting();
             app.UseEndpoints(endpoints =>
             {
